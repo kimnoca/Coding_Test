6a3946e18512c3139ad425a2a68fd0fb98b9b2ac
kimnoca <88851709+kimnoca@users.noreply.github.com>
2023-07-20 09:04:38 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -1,3 +1,39 @@
+## 뭔가 문제인지 알았음
 def solution(genres, plays):
     answer = []
+    dict = {}
+    for i in range(len(genres)):
+        if genres[i] not in dict:
+            list = []
+            list.append((i, plays[i]))
+            dict[genres[i]] = list
+        else:
+            list = dict.get(genres[i])
+            list.append((i, plays[i]))
+            dict[genres[i]] = list
+    for key in dict.keys():
+        list = dict.get(key)
+        list.sort(key=lambda x: (-x[1], x[0]))  # key point
+        dict[key] = list
+    sum_dict = {}
+    for key in dict.keys():
+        list = dict.get(key)
+        sum = 0
+        for i in range(len(list)):
+            sum += list[i][1]
+        sum_dict[key] = sum
+    sum_dict = sorted(sum_dict.items(), key=lambda x: -x[1])
+    for key, v in sum_dict:
+        list = dict.get(key)
+        stop = 0
+        for i in range(len(list)):
+            if stop == 2:
+                break
+            if len(list) == 1:
+                answer.append(list[i][0])
+                break
+            else:
+                answer.append(list[i][0])
+            stop += 1
+
     return answer
