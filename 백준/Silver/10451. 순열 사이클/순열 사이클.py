45ac8d22dae802a21feb96814efd1aca18349309
kimnoca <88851709+kimnoca@users.noreply.github.com>
2023-07-26 11:23:24 +0900

    [Silver III] Title: 순열 사이클, Time: 620 ms, Memory: 31364 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.py
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -14,15 +14,14 @@ def dfs(v):
 T = int(input())
 
 for _ in range(T):
-    count = -1
+    count = 0
     N = int(input())
     graph = [[] for _ in range(N + 1)]
     visited = [False] * (N + 1)
     data = list(map(int, input().split()))
     for i in range(1, N + 1):
         graph[i].append(data[i - 1])
-        graph[data[i - 1]].append(i)
-    for i in range(N + 1):
+    for i in range(1, N + 1):
         if not visited[i]:
             dfs(i)
             count += 1
