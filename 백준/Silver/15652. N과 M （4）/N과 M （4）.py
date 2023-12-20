3bfb79be4ce288b4885b31f83b94296c0fdfd2a0
kimnoca <88851709+kimnoca@users.noreply.github.com>
2023-12-20 15:16:25 +0900

    [Silver III] Title: N과 M (4), Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+
+result = []
+
+
+def dfs(start):
+    if len(result) == m:
+        print(*result)
+        return
+
+    for i in range(start, n + 1):
+        result.append(i)
+        dfs(i)
+        result.pop()
+
+
+dfs(1)
