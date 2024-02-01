8b80c21cc471112a148cfd54b3b7cdc8ca623018
kimnoca <88851709+kimnoca@users.noreply.github.com>
2024-02-01 10:51:34 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,23 +1,6 @@
 def solution(genres, plays):
     answer = []
     genres_set = set(genres)
-    genres_index_map = {}
-    
-    for i in range(len(genres)):
-        if genres[i] in genres_index_map:
-            temp = [*(genres_index_map[genres[i]]), i]
-            genres_index_map[genres[i]] = temp
-        else:
-            genres_index_map[genres[i]] = [i]    
-    
-    genres_plays_sum = dict.fromkeys(genres_set, 0)
-    
-    for gen in genres_set:
-        for i in genres_index_map[gen]:
-            genres_plays_sum[gen] += plays[i]
-            
-    ranks = sorted(genres_plays_sum.items(), key = lambda x : x[1], reverse=True)
-    
     genres_plays_counts = {}
     
     for i in range(len(genres)):
@@ -26,9 +9,14 @@ def solution(genres, plays):
             genres_plays_counts[genres[i]] = temp
         else:
             genres_plays_counts[genres[i]] = [(plays[i], i)]
-    print(genres_plays_counts)
     
+    genres_plays_sum = dict.fromkeys(genres_set, 0)
+    
+    for gen in genres_set:
+        for play, index in genres_plays_counts[gen]:
+            genres_plays_sum[gen] += play
             
+    ranks = sorted(genres_plays_sum.items(), key = lambda x : x[1], reverse=True)
     for rank, _ in ranks:
         temp = sorted(genres_plays_counts[rank], key = lambda x : -x[0])
         for i in range(2):
@@ -36,6 +24,5 @@ def solution(genres, plays):
                 answer.append(temp[i][1])
                 break
             answer.append(temp[i][1])
-            
-        
+                    
     return answer
