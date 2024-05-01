0b0b1eaada6cd35e71307cc800357133545336d2
kimnoca <88851709+kimnoca@users.noreply.github.com>
2024-05-01 09:56:59 +0900

    [level 2] Title: 더 맵게, Time: 1434.93 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,19 +1,14 @@
 import java.util.*;
 
 class Solution {
-    public boolean checkQueue(PriorityQueue<Integer> queue, int K) {
-        for (Integer i : queue) {
-            if (i < K) return false;
-        }
-        return true;
-    }
+
     public int solution(int[] scoville, int K) {
         int answer = 0;
         PriorityQueue<Integer> queue = new PriorityQueue<>();
         for (Integer i : scoville) {
             queue.add(i);
         }        
-        while (!checkQueue(queue, K)) {
+        while (!(queue.peek() >= K)) {
             if (queue.size() == 1) {
                 return -1;
             }
