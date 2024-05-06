e14017f4be57aa338dfd774665a22b6e1527f7e1
kimnoca <88851709+kimnoca@users.noreply.github.com>
2024-05-06 23:51:32 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 660 ms, Memory: 63100 KB -BaekjoonHub

---
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> startQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> endQueue = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            String[] split = input.split(" ");
+
+            int start = Integer.parseInt(split[0]);
+            int end = Integer.parseInt(split[1]);
+
+            startQueue.add(start);
+            endQueue.add(end);
+        }
+
+        int answer = 1;
+        int start = startQueue.poll();
+        int end = endQueue.poll();
+
+        while (!startQueue.isEmpty()) {
+
+            start = startQueue.poll();
+
+            if (start < end) {
+                answer++;
+            } else {
+                end = endQueue.poll();
+            }
+        }
+        System.out.println(answer);
+    }
+}
