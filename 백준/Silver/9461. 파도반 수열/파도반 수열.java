e341507ea233dc38726d9e10d2561dc1ceec3a5f
kimnoca <88851709+kimnoca@users.noreply.github.com>
2024-05-16 11:16:24 +0900

    [Silver III] Title: 파도반 수열, Time: 84 ms, Memory: 11556 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        long[] dp = new long[101];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+        dp[6] = 3;
+        dp[7] = 4;
+        dp[8] = 5;
+        dp[9] = 7;
+        dp[10] = 9;
+        dp[11] = 12;
+        for (int i = 11; i <= 100; i++) {
+            dp[i] = dp[i - 1] + dp[i - 5];
+        }
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+    }
+}
