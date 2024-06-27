63a223e829344cdb2d3ce50f2bb913684686d37d
kimnoca <88851709+kimnoca@users.noreply.github.com>
2024-06-27 14:23:28 +0900

    [level 3] Title: 단어 변환, Time: 0.05 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    static boolean[] visited;
+    static int answer = 0;
+    public int solution(String begin, String target, String[] words) {
+        HashSet<String> wordSet = new HashSet<>();
+        
+        for (String word : words) {
+            wordSet.add(word);
+        }
+        
+        if (!wordSet.contains(target)) {
+            return 0;
+        }
+        visited = new boolean[words.length];
+        
+        dfs(begin, target, words, 0);
+        
+        
+        return answer;
+    }
+    public void dfs(String start, String target, String[] words,int count) {
+        if (start.equals(target)) {
+            answer = count;
+        }
+        
+        for (int i = 0; i < words.length; i++) {
+            if (!visited[i] && check(start, words[i])) {
+                visited[i] = true;
+                dfs(words[i], target, words, count + 1);
+            }
+        }
+    }
+    public boolean check(String a, String b) {
+        int check = 0;
+        for (int i = 0; i < a.length(); i ++) {
+            if (a.charAt(i) == b.charAt(i)) {
+                check ++;
+            }
+        }
+        if (check  == a.length() -1) {
+            return true;
+        }
+        return false;
+    }
+}
