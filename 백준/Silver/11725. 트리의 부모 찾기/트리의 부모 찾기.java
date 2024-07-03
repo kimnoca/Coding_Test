7dec184c0db5cd7042d646e5524fedbaa04af442
kimnoca <88851709+kimnoca@users.noreply.github.com>
2024-07-03 10:22:28 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1972 ms, Memory: 90512 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<ArrayList<Integer>> map = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            map.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            String[] input = br.readLine().split(" ");
+            int x = Integer.parseInt(input[0]);
+            int y = Integer.parseInt(input[1]);
+
+            map.get(x).add(y);
+            map.get(y).add(x);
+
+        }
+
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n + 1];
+        int[] answers = new int[n + 1];
+
+        queue.add(new int[]{1, 1});
+        visited[1] = true;
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+
+            int start = now[0];
+            int end = now[1];
+
+            for (Integer i : map.get(end)) {
+                if (!visited[i]) {
+                    queue.add(new int[]{end, i});
+                    visited[i] = true;
+                }
+            }
+
+            if (visited[start]) {
+                answers[end] = start;
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            System.out.println(answers[i]);
+        }
+
+    }
+}
