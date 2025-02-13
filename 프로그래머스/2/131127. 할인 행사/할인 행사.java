09105834514bfef15779f4831268dbf439fbbde1
kimnoca <88851709+kimnoca@users.noreply.github.com>
2025-02-13 16:10:50 +0900

    [level 2] Title: 할인 행사, Time: 106.11 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+    
+        HashMap<String, Integer> wantMap = new HashMap();
+        
+        for (int i = 0; i < want.length; i++) {
+            wantMap.put(want[i], number[i]);
+        }
+        
+        
+        
+        for (int i = 0; i <= discount.length - 10; i++) {
+            HashMap<String, Integer> buyMap = new HashMap();
+            for (int k = i; k < i + 10; k++) {
+                int count = buyMap.getOrDefault(discount[k], 0) + 1;
+                buyMap.put(discount[k], count);
+            }
+            int buyItemCount = 0;
+            
+            
+            for (String key : wantMap.keySet()) {
+                if (wantMap.get(key) == buyMap.getOrDefault(key, 0)) {
+                    buyItemCount ++;
+                }
+            }
+            
+            // System.out.println(buyItemCount);
+            
+            if (buyItemCount == want.length) {
+                answer ++;
+            }
+        }
+        
+        return answer;
+    }
+}
