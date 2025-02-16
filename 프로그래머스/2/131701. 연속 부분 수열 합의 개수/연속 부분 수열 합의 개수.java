33d246e57219184bae212e0ccf30994de866ab45
kimnoca <88851709+kimnoca@users.noreply.github.com>
2025-02-16 22:23:44 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 3590.44 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    static Set<Integer> result = new HashSet<>();
+    
+    public int solution(int[] elements) {
+        
+        for (int e : elements) {
+            result.add(e);
+        }
+        
+        for(int i = 2; i <= elements.length; i++) {
+            for (int k = 0; k < elements.length; k ++) {
+                 dfs(elements[k], i, 1, elements, k);
+            }
+            // System.out.println(result + ":" + i);
+        }
+        
+        
+        // System.out.println(result);
+//         
+        
+        return result.size();
+    }
+    
+    public void dfs(int n, int size, int depth, int[] elements, int start) {
+        
+        
+        if (size == depth) {
+            result.add(n);
+            return;
+        }
+
+        dfs(n + elements[(start + 1) % elements.length], size, depth + 1, elements, start + 1);
+    }
+}
